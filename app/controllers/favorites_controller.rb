fe1977b21c493f55b7859dd9ee57602b76507fe2
nhana <xhuaze914@gmail.com>
2022-03-08 03:40:58 +0000

    コードの一部修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,8 +3,7 @@ class FavoritesController < ApplicationController
     # こう記述することで、「current_userに関連したFavoriteクラスの新しいインスタンス」が作成可能。
     # つまり、favorite.user_id = current_user.idが済んだ状態で生成されている。
     # buildはnewと同じ意味で、アソシエーションしながらインスタンスをnewする時に形式的に使われる。
-    favorite = current_user.favorites.build(tweet_id: params[:tweet_id])
-    favorite.save
+    favorite = current_user.favorites.create(tweet_id: params[:tweet_id])
     redirect_to tweets_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,7 @@ Rails.application.routes.draw do
   root 'tweets#index'
     
   devise_for :users
-  
   resources :tweets do
-    
     resource :favorites, only: [:create, :destroy]
   end
   
